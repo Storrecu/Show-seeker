57fcd1cbe3028c085b078db7f534ef8220d441d4
Silvia <silviatc1993@gmail.com>
2023-10-02 09:08:09 +0200

    fixed button erase all in favorites list

--- src/js/0_main.js
+++ src/js/0_main.js
@@ -2,21 +2,18 @@
 /* eslint-disable no-unused-vars */
 'use strict';
 
-//VARIABLES:
 //Elementos de HTML que nos traemos a JS:
 const input = document.querySelector('.js-input-search'); // casilla de búsqueda
 const btnSearch = document.querySelector('.js-btn-search'); // botón para buscar
 const listForm = document.querySelector('.js-form-list'); // lista que muestra la serie que has buscado
 const listFav = document.querySelector('.js-fav-list'); // lista que almacenará los favoritos
-const btnDelete = document.querySelector('.js-delete-fav');
+const btnDelete = document.querySelector('.js-delete-fav'); // botón para eliminar lista de favoritos
 
 //Otras variables:
-const URL = '//api.tvmaze.com/search/shows?q='; //URL de la API
 let favList = []; //lista de favoritos
 let showsList = []; // lista de resultados
 
-//LocalStorage:
-/* Con este bloque de código, compruebo al arrancar la página, que hayan datos en el local stroage. favList es la variable que tiene la lista de favoritos. Cuando storedFavorites tiene datos, los copiamos a favList y luego llamamos a la función renderFavouriteShows(favList) para mostrarlos en el HTML */
+// NOTE:Con este bloque de código, compruebo al arrancar la página, que hayan datos en el local stroage
 const storedFavorites = JSON.parse(localStorage.getItem('favorites'));
 if (storedFavorites) {
   favList = storedFavorites;
@@ -25,8 +22,15 @@ if (storedFavorites) {
   renderShow();
 }
 
-//FUNCIONES:
-//Función para pintar la lista de resultados:
+// Función arrow con la que llamamos a todos los li con clase js-card y se les añade evento listener tipo click.
+const addToFavourites = () => {
+  const allShows = document.querySelectorAll('.js-card');
+  for (const item of allShows) {
+    item.addEventListener('click', handleClickShow);
+  }
+};
+
+// :Función para pintar la lista de resultados:
 function renderShow() {
   const inputValue = input.value;
   const URL = `//api.tvmaze.com/search/shows?q=${inputValue}`;
@@ -36,12 +40,12 @@ function renderShow() {
       .then((response) => response.json())
       .then((data) => {
         showsList = data;
-        //Compruebo que lo buscado coincide con los datos que contiene la respuesta
+        // NOTE:Compruebo que lo buscado coincide con los datos que contiene la respuesta
         listForm.innerHTML = '';
         if (data.length === 0) {
-          listForm.innerHTML = 'No matches found.Try a diferent TV show';
+          listForm.innerHTML = 'No matches found.Try a diferent show';
         } else {
-          //En caso que lo que busco, coincida con la info que tiene la respuesta lo pinto en el HTML
+          // NOTE:En caso que lo que busco, coincida con la info que tiene la respuesta lo pinto en el HTML
           listForm.innerHTML = '';
           for (const item of showsList) {
             let showElement = '';
@@ -60,11 +64,11 @@ function renderShow() {
         }
       });
   } else {
-    listForm.innerHTML = 'Write any TV show to start';
+    listForm.innerHTML = 'Write any show to start';
   }
 }
 
-//Función para pintar la lista de favoritos:
+// Función para pintar la lista de favoritos:
 function renderFavouriteShows(favoritesShows) {
   listFav.innerHTML = '';
   for (const item of favoritesShows) {
@@ -83,15 +87,7 @@ function renderFavouriteShows(favoritesShows) {
   }
 }
 
-//Arrow functions para añadir eventos a elementos dinámicos:
-//llamamos a todos los div con clase js-card, se les añade evento listener tipo click.
-const addToFavourites = () => {
-  const allShows = document.querySelectorAll('.js-card');
-  for (const item of allShows) {
-    item.addEventListener('click', handleClickShow);
-  }
-};
-//Función con la que añadimos evento listener a las X de las series de favoritos
+// Función arrow con la que añadimos evento listener a las X de las series de favoritos
 const crossDeleteFavs = () => {
   const deletAllFavs = document.querySelectorAll('.js-fav-cross');
   for (const item of deletAllFavs) {
@@ -100,19 +96,20 @@ const crossDeleteFavs = () => {
 };
 crossDeleteFavs();
 
-//FUNCIONES MANEJADORAS:
+// Función manejadora del botón 'Erase all'
 function handleDeleteBtn() {
   listFav.innerHTML = '';
+  favList = [];
   localStorage.clear();
 }
 
-//Función manejadora del botón de "buscar":
+// Función manejadora del botón de "Search"
 function handleSearchButton(event) {
   event.preventDefault();
   renderShow();
 }
 
-//Función manejadora divs dinámicos
+// Función manejadora li dinámicos
 function handleClickShow(event) {
   event.preventDefault();
   const idShow = event.currentTarget.id;
@@ -120,32 +117,28 @@ function handleClickShow(event) {
   const indexFav = favList.findIndex(
     (item) => item.show.id === foundShow.show.id
   );
-  //Si no está (-1) lo añade
   if (indexFav === -1) {
     favList.push(foundShow);
-    //Si está lo eliminará
   } else {
     favList.splice(indexFav, 1);
   }
-  event.currentTarget.classList.toggle('selected');
-  localStorage.setItem('favorites', JSON.stringify(favList)); // guardamos en local storage las series añadidas a favoritos. La llamamos aquí porque es en este punto donde de generan cambios en la lista de favoritos
+  localStorage.setItem('favorites', JSON.stringify(favList)); // NOTE: guardamos en local storage las series añadidas a favoritos. La llamo aquí porque es en este punto donde se generan cambios en la lista de favoritos
   renderFavouriteShows(favList);
 }
 
-//Función manejadora X dinámica
+// Función manejadora X dinámica
 function handleClickDelete(event) {
   if (event.target.classList.contains('js-fav-cross')) {
     event.target.parentElement.remove();
   }
 }
-btnDelete.addEventListener('click', handleDeleteBtn);
 
-//Función manejadora del input, para que el mensaje de error no esté siempre visible
+// Función manejadora del input, para que el mensaje de error no esté siempre visible
 function handleInput() {
   listForm.innerHTML = '';
 }
 
-//Eventos:
+// Eventos
 btnDelete.addEventListener('click', handleDeleteBtn);
 btnSearch.addEventListener('click', handleSearchButton);
 input.addEventListener('change', handleInput);
